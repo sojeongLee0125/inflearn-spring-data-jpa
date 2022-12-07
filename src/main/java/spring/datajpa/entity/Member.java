10c7f4b1d79d018c58e428808f779f119af9c57f
sojeonglee <sojeonglee0125@gmail.com>
2022-12-07 23:05:53 +0900

    feat : 순수 jpa로 커스텀 쿼리메소드 생성

--- src/main/java/spring/datajpa/entity/Member.java
+++ src/main/java/spring/datajpa/entity/Member.java
@@ -33,6 +33,11 @@ public class Member {
         }
     }
 
+    public Member(String name, int age) {
+        this.username = name;
+        this.age = age;
+    }
+
     public void changeTeam(Team team) {
         this.team = team;
         team.getMembers().add(this);
