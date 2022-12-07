8272947f8af3c8602227f5179731f81fe7b0db55
sojeonglee <sojeonglee0125@gmail.com>
2022-12-07 23:08:11 +0900

    feat : 스프링 데이터 jpa를 활용한 쿼리 메서드 생성 + 테스트 코드

--- src/main/java/spring/datajpa/repository/MemberRepository.java
+++ src/main/java/spring/datajpa/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package spring.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import spring.datajpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
